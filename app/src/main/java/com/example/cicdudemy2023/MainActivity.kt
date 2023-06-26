7cf680af7ec5f2ec51470508ccd5dba42e4b8309
Teresa Fernández <teresa.fernandez@movetia.com>
2023-06-26 12:14:51 +0200

    Add feature-1 fromandroid studio

--- app/src/main/java/com/example/cicdudemy2023/MainActivity.kt
+++ app/src/main/java/com/example/cicdudemy2023/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("CI/CD World")
+                    Greeting("CI/CD World feature-1")
                 }
             }
         }
